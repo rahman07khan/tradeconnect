17e069122b11ccd4222893f38f465d8ada994757
vicky <vigneshganapathi1006@gmail.com>
2024-09-19 18:44:38 +0530

    [MOD]getproductbyuser

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from orders.views import CategoryView,ProductView,CartItemUserApi
+from orders.views import CategoryView,ProductView,CartItemUserApi,GetProductBySeller
 
 urlpatterns = [
     path('categories/',CategoryView.as_view(),name='categories'),
     path('products/',ProductView.as_view(),name='products'),
-    path('cart/',CartItemUserApi.as_view(),name='cart')
+    path('cart/',CartItemUserApi.as_view(),name='cart'),
+    path('specific/',GetProductBySeller.as_view(),name='specific'),
 ]
