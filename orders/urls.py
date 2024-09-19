4f1749583edbb7ee78027160b6bafdbeb487431e
vish140503 <vishalakshik14503@gmail.com>
2024-09-19 18:21:32 +0530

    [MOD]

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,5 @@ from orders.views import CategoryView,ProductView,CartItemUserApi
 urlpatterns = [
     path('categories/',CategoryView.as_view(),name='categories'),
     path('products/',ProductView.as_view(),name='products'),
-     path('cart/',CartItemUserApi.as_view(),name='cart')
+    path('cart/',CartItemUserApi.as_view(),name='cart')
 ]
